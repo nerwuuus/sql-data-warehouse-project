b99f6842f0691467aa6c2d692e684109de247df0
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-08-26 12:59:30 +0200

    Create data_insert.sql

---
+++ scripts/data_insert.sql
@@ -0,0 +1,27 @@
+-- Insert data into CRM tables
+COPY bronze.crm_cust_info
+FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\Data Analytics\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH (
+    FORMAT csv,
+    HEADER true,
+    DELIMITER ',',
+    ENCODING 'UTF8'
+);
+
+/* 
+
+=====================================
+            
+            WARNING !!!
+
+=====================================
+Error 'Permission denied' while uploading data. Paste the code below in pgAdmin4, PSQL Tool to load data manually:
+
+\copy bronze.crm_cust_info FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\Data Analytics\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\cust_info.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy bronze.crm_prd_info FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\Data Analytics\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\prd_info.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy bronze.crm_sales_details FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\Data Analytics\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\sales_details.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy bronze.erp_cust_az12 FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\Data Analytics\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy bronze.erp_loc_a101 FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\Data Analytics\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy bronze.erp_px_cat_g1v2 FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\Data Analytics\SQL\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+*/
