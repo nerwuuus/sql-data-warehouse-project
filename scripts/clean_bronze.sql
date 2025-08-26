a2e3684c9b7493d07c9b845f8cac502074aa5345
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-08-26 13:19:10 +0200

    Create clean_bronze.sql

---
+++ scripts/clean_bronze.sql
@@ -0,0 +1,14 @@
+-- Create a stored procedure for removing the content of tables
+CREATE OR REPLACE FUNCTION bronze.clean_bronze()
+RETURNS void
+LANGUAGE plpgsql
+AS $$
+BEGIN
+        TRUNCATE TABLE bronze.crm_cust_info;
+        TRUNCATE bronze.crm_prd_info;
+        TRUNCATE bronze.crm_sales_details;
+        TRUNCATE bronze.erp_cust_az12;
+        TRUNCATE bronze.erp_loc_a101;
+        TRUNCATE bronze.erp_px_cat_g1v2;
+END;
+$$;
