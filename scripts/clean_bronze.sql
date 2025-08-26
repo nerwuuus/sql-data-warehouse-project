3c84b31dc5156fa9dc75b96ab8cb5bd189edb057
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-08-26 13:28:13 +0200

    Update clean_bronze.sql

--- scripts/clean_bronze.sql
+++ scripts/clean_bronze.sql
@@ -4,12 +4,14 @@ RETURNS void
 LANGUAGE plpgsql
 AS $$
 BEGIN
-        TRUNCATE TABLE bronze.crm_cust_info;
-        TRUNCATE bronze.crm_prd_info;
-        TRUNCATE bronze.crm_sales_details;
-        TRUNCATE bronze.erp_cust_az12;
-        TRUNCATE bronze.erp_loc_a101;
-        TRUNCATE bronze.erp_px_cat_g1v2;
+    TRUNCATE TABLE bronze.crm_cust_info;
+    TRUNCATE bronze.crm_prd_info;
+    TRUNCATE bronze.crm_sales_details;
+    TRUNCATE bronze.erp_cust_az12;
+    TRUNCATE bronze.erp_loc_a101;
+    TRUNCATE bronze.erp_px_cat_g1v2;
+
+    RAISE NOTICE 'Bronze tables have been successfully truncated.';
 END;
 $$;
 
